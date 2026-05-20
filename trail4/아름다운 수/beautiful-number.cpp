eb1ea6a6344ee0b7fcae1acfaa59ffccdfec7a56
hyunjun <86935947+hyjuy@users.noreply.github.com>
2026-05-20 15:14:15 +0900

    [Passed] 아름다운 수 (2ms, 0MB)

--- trail4/아름다운 수/beautiful-number.cpp
+++ trail4/아름다운 수/beautiful-number.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
-#include <string>
-#include <vector>
+
 using namespace std;
+
 int n;
-vector <int> v;
 int ans = 0;
 void BeautifulNum(int curr_num){
     if(curr_num == n){
@@ -11,21 +10,14 @@ void BeautifulNum(int curr_num){
         return;
     }
     for(int i = 1; i <= 4; i++){
-        if(curr_num + i - 1 >= n) continue;
-        for(int j = 0; j < i; j++){
-            v.push_back(i);
-        }
+        if(curr_num + i - 1 > n )continue;
         BeautifulNum(curr_num + i);
-        for(int j = 0; j < i; j++){
-            v.pop_back();
-        }
     }
 }
-
-int main() {
-    // Please write your code here.
+int main(void){
     cin >> n;
+
     BeautifulNum(0);
+
     cout << ans;
-    return 0;
 }
