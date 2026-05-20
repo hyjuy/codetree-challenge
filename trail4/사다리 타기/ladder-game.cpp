fd3bd625a15d253c961287b5eb4b1b7a3122eba0
hyunjun <86935947+hyjuy@users.noreply.github.com>
2026-05-20 19:49:14 +0900

    [Passed] 사다리 타기 (2ms, 0MB)

--- trail4/사다리 타기/ladder-game.cpp
+++ trail4/사다리 타기/ladder-game.cpp
@@ -1,116 +1,63 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
 using namespace std;
 
-struct Node{
-    int r, c;
-};
-
-struct Info{
-    int c1, c2;
-};
-
 int n, m;
-int fir_arr[12] = {0,};
-int temp_arr[12] = {0,};
-
-vector<Info> v[16];
-vector<Node> list;
-vector<int> sel_list;
-
+vector <pair<int, int>> lines;
+vector <pair<int, int>> selected_lines;
 int ans = 10000000;
 
-void move(int idx, vector<Info> ladder[16], int mode){
-    int c = idx;
+bool Possible(){
+    int num1[12] = {0,};
+    int num2[12] = {0,};
 
-    // r의 최대 범위가 15라면 15까지 확인
-    for(int r = 1; r <= 15; r++){
-        for(auto node : ladder[r]){
-            if(c == node.c1){
-                c = node.c2;
-                break;
-            }
-            else if(c == node.c2){
-                c = node.c1;
-                break;
-            }
-        }
+    for(int i = 1; i <= n; i++){
+        num1[i - 1] = num2[i - 1] = i;
     }
 
-    if(mode == 0) {
-        fir_arr[c] = idx;
+    for(int i = 0; i < (int)lines.size(); i++){
+        int idx = lines[i].second;
+        swap(num1[idx], num1[idx+1]);
     }
-    else {
-        temp_arr[c] = idx;
+
+    for(int i = 0; i < (int)selected_lines.size(); i++){
+        int idx = selected_lines[i].second;
+        swap(num2[idx], num2[idx+1]);
     }
+
+    for(int i = 1; i <= n; i++){
+        if(num1[i] != num2[i])return false;
+    }
+
+    return true;
 }
-
-void Choose(int curr_idx){
-    // 이미 현재 정답보다 많이 골랐다면 더 볼 필요 없음
-    if((int)sel_list.size() >= ans) {
+void FindMinLines(int cnt){
+    if(cnt == m){
+        if(Possible()){
+            ans = min(ans, (int)selected_lines.size());         
+        }
         return;
     }
 
-    if(curr_idx == (int)list.size()){
-        vector<Info> temp_v[16];
-
-        for(int i = 0; i < (int)sel_list.size(); i++){          
-            int r = list[sel_list[i]].r;
-            int c = list[sel_list[i]].c;
-
-            temp_v[r].push_back({c, c + 1});
-        }
-
-        // temp_arr 초기화
-        for(int i = 1; i <= n; i++){
-            temp_arr[i] = 0;
-        }
-
-        // 선택한 가로줄만으로 사다리 결과 계산
-        for(int i = 1; i <= n; i++){
-            move(i, temp_v, 1);
-        }
-
-        // 원본 사다리 결과와 같은지 비교
-        for(int i = 1; i <= n; i++){
-            if(temp_arr[i] != fir_arr[i]){
-                return;
-            }
-        }
-
-        if(ans > (int)sel_list.size()){
-            ans = sel_list.size();
-        }
-
-        return;
-    }
-
-    // 현재 가로줄 선택
-    sel_list.push_back(curr_idx);
-    Choose(curr_idx + 1);
-    sel_list.pop_back();
-
-    // 현재 가로줄 미선택
-    Choose(curr_idx + 1);
+    selected_lines.push_back(lines[cnt]);
+    FindMinLines(cnt+1);
+    selected_lines.pop_back();
+    FindMinLines(cnt+1);
 }
-
 int main() {
     cin >> n >> m;
 
-    for(int i = 0; i < m; i++){
+    for (int i = 0; i < m; i++) {
         int r, c;
         cin >> c >> r;
-
-        list.push_back({r, c});
-        v[r].push_back({c, c + 1});
+        lines.push_back({r,c});
     }
 
-    // 원본 사다리 결과 계산
-    for(int i = 1; i <= n; i++){
-        move(i, v, 0);
-    }
+    sort(lines.begin(), lines.end());
 
-    Choose(0);
+    FindMinLines(0);
 
     cout << ans;
 
