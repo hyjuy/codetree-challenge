11b6a12e7bdbfe920033139b24984815c289b645
hyunjun <86935947+hyjuy@users.noreply.github.com>
2026-05-30 21:19:39 +0900

    [Passed] 체크판위에서 2 (2ms, 0MB)

---
+++ trail2/체크판위에서 2/on-the-checkboard-2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int R, C; 
+char map[15][15];
+int ans = 0;
+
+void findPath(int r, int c, int step){
+    if(r == R - 1 && c == C - 1 && step == 3){
+        ans++;
+        return;
+    }
+    if(r == R - 1 || c == C - 1)return;
+
+    char cur = map[r][c];
+
+    for(int i = r + 1; i < R; i++){
+        for(int j = c + 1; j < C; j++){
+            if((cur == 'W' && map[i][j] == 'B') || (cur == 'B' && map[i][j] == 'W')){
+                findPath(i,j,step+1);
+            }
+        }
+    }
+}
+int main() {
+    // Please write your code here.
+    //적어도 한칸 이상 오른쪽, 적어도 한칸 이상 아래쪽
+    //시작 -> 1칸 -> 2칸 -> 마지막
+
+    cin >> R >> C;
+
+    for(int i = 0; i < R; i++){
+        for(int j = 0; j < C; j++){
+            cin >> map[i][j];
+        }
+    }
+    findPath(0,0,0);
+    cout << ans;
+    return 0;
+}
